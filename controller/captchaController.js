c1a273b47249aa86d86250ea9a458956b1046815
Sanchit Thakur <jarvus92001@gmail.com>
2024-11-17 20:53:08 +0530

    remove commit

--- controller/captchaController.js
+++ controller/captchaController.js
@@ -2,20 +2,20 @@ const svgCaptcha = require("svg-captcha");
 const User = require("../model/userModel");
 const Captcha = require("../model/capchaModel");
 
-let captchas = {}; // Temporary store for captchas
+let captchas = {}; 
 
-//generateCaptcha
+
 exports.generateCaptcha = (req, res) => {
   const captcha = svgCaptcha.create();
   const captchaId = Date.now().toString();
   captchas[captchaId] = captcha.text;
 
-  setTimeout(() => delete captchas[captchaId], 60000); // Remove after 1 minute
+  setTimeout(() => delete captchas[captchaId], 60000); 
 
   res.status(200).json({ id: captchaId, svg: captcha.data });
 };
 
-//verifyCaptcha
+
 exports.verifyCaptcha = async (req, res) => {
   const { userId, captchaId, input } = req.body;
 
@@ -36,11 +36,11 @@ exports.verifyCaptcha = async (req, res) => {
 
   res.status(200).json({ message: "Correct CAPTCHA", coins: user.coins });
 
-  // Automatically Replace Expired CAPTCHAs
+
   exports.replaceExpiredCaptchas = async () => {
     try {
       const expiredCaptchas = await captchas.find({
-        createdAt: { $lte: new Date(Date.now() - 60000) }, // Captchas older than 60s
+        createdAt: { $lte: new Date(Date.now() - 60000) }, 
       });
 
       for (const expiredCaptcha of expiredCaptchas) {
--- model/capchaModel.js
+++ model/capchaModel.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const captchaSchema = new mongoose.Schema({
-  _id: String, // Use unique ID for CAPTCHA
+  _id: String, 
   text: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }, // Add timestamp
+  createdAt: { type: Date, default: Date.now }, 
 });
 
-captchaSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 }); // TTL index
+captchaSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 }); 
 
 module.exports = mongoose.model("Captcha", captchaSchema);
